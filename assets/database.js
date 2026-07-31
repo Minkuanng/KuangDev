f5c32da0818530f4453dd77658a219611b7c7e15
Minkuan <qn781159@gmail.com>
2026-08-01 03:03:27 +0700

    Configure and initialize Firebase in database.js
    Added Firebase initialization and export functionality.

--- assets/database.js
+++ assets/database.js
@@ -1,11 +1,24 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// assets/database.js
+
+// Cấu hình Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCPDDRt520fSynXCGrr2aF-KvpnS_ZuIm8",
-  authDomain: "shop-c6777.firebaseapp.com",
-  databaseURL: "https://shop-c6777-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "shop-c6777",
-  storageBucket: "shop-c6777.firebasestorage.app",
-  messagingSenderId: "43186563268",
-  appId: "1:43186563268:web:0d392ba57039a919670191",
-  measurementId: "G-445DCB6NV0"
+    apiKey: "AIzaSyCPDDRt520fSynXCGrr2aF-KvpnS_ZuIm8",
+    authDomain: "shop-c6777.firebaseapp.com",
+    databaseURL: "https://shop-c6777-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "shop-c6777",
+    storageBucket: "shop-c6777.firebasestorage.app",
+    messagingSenderId: "43186563268",
+    appId: "1:43186563268:web:0d392ba57039a919670191",
+    measurementId: "G-445DCB6NV0"
 };
+
+// Khởi tạo Firebase (chỉ chạy 1 lần)
+if (typeof firebase !== 'undefined' && !firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    console.log('✅ Firebase initialized from database.js');
+}
+
+// Export để dùng ở các file khác (nếu cần)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { firebaseConfig };
+}
