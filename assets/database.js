250788c10130ef6d2dddebec12a4880912d86c98
Minkuan <qn781159@gmail.com>
2026-08-01 02:58:50 +0700

    Create database.js

---
+++ assets/database.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPDDRt520fSynXCGrr2aF-KvpnS_ZuIm8",
+  authDomain: "shop-c6777.firebaseapp.com",
+  databaseURL: "https://shop-c6777-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shop-c6777",
+  storageBucket: "shop-c6777.firebasestorage.app",
+  messagingSenderId: "43186563268",
+  appId: "1:43186563268:web:0d392ba57039a919670191",
+  measurementId: "G-445DCB6NV0"
+};
